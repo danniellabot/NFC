5edce0722c97dca8d4c78664352eb4c8e51d834c
Tom Page <tompage45@gmail.com>
2022-07-07 13:46:42 +0100

    Added logic for getting geolocation and address for that location

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {StyleSheet, View, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, View, SafeAreaView, Text, PermissionsAndroid, Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {MessageRequest} from './Assistant';
 import WatsonIcon from './WatsonIcon';
 import {GiftedChat} from 'react-native-gifted-chat';
+import Geocoder from 'react-native-geocoding';
+
+const GOOLE_API_KEY = "AIzaSyAdr5PRXhg-kNQGXr-D3-WtiUoGT2PAWDY";
+Geocoder.init(GOOLE_API_KEY);
+
+import Geolocation from 'react-native-geolocation-service';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -18,6 +25,7 @@ export default class App extends React.Component {
   componentDidMount() {
     console.log('CAN ACCESS STATE', this.state);
     this.initalMessage();
+    this.getLocation();
   }
 
   render() {
@@ -35,6 +43,7 @@ export default class App extends React.Component {
           <Text style={styles.headerText}>Chat with Watson</Text>
         </View>
         <GiftedChat
+          textInputStyle={{color: '#000'}}
           placeholder="Send your message to Watson..."
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
@@ -124,6 +133,110 @@ export default class App extends React.Component {
   renderAvatar = () => {
     return <WatsonIcon />;
   };
+
+  reverseGeocode = async (latitude, longitude) => {
+    try {
+      const result = await Geocoder.from({latitude,longitude})
+      return this.parseAddresses(result);
+    } catch (error) {
+      console.log("Error getting addresses ", error);
+    }
+  }
+
+  parseAddresses = (addressResults) => {
+    const appendAddressLine = (exsitingPart, newPart) => {
+      if (!exsitingPart.lenght) return newPart;
+      return exsitingPart + " " + newPart;
+    };
+
+    const parseSingleAddress = (addressParts) => {
+      return addressParts.reduce((addressObj, addressPart) => {
+        const types = addressPart.types
+        if (types.includes("street_number")) {
+          addressObj.addressLine1 = appendAddressLine(addressObj.addressLine1, addressPart.long_name);
+        } else if (types.includes("route")) {
+          addressObj.addressLine1 = appendAddressLine(addressObj.addressLine1, addressPart.long_name);
+        } else if (types.includes("subpremise")) {
+          addressObj.addressLine1 = appendAddressLine(addressObj.addressLine1, addressPart.long_name);
+        } else if (types.includes("administrative_area_level_2")) {
+          addressObj.addressLine2 = addressPart.long_name;
+        } else if (types.includes("postal_code")) {
+          addressObj.postcode = addressPart.long_name;
+        } else if (types.includes("postal_town")) {
+          addressObj.town = addressPart.long_name;
+        } else if (types.includes("country")) {
+          addressObj.country = addressPart.long_name;
+        }
+        return addressObj;
+      }, {
+        addressLine1: "",
+        addressLine2: "",
+        town: "",
+        postcode: "",
+        country: ""
+      })
+    };
+
+    return addressResults.results
+      .filter(({types}) => (types.includes("street_address") || types.includes("premise")))
+      .map(result => {
+        return {
+          formatted: result.formatted_address,
+          address: parseSingleAddress(result.address_components)
+        }
+      });
+  }
+
+  requestLocationPermission = async () => {
+    if (Platform.OS === "ios") { 
+      return true;
+    }
+
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: "NFC App Location Permisson",
+          message: "NFC App would like access to your location",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("location granted");
+        return true;
+      } else {
+        console.log("location permission denied");
+        return false;
+      }
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  getLocation = () => {
+    this.requestLocationPermission().then((locationPermissinon) => {
+      if (locationPermissinon) {
+        Geolocation.getCurrentPosition(
+            (position) => {
+              console.log("POSITION", position);
+              this.reverseGeocode(position.coords.latitude, position.coords.longitude).then(addresses => {
+                console.log("ADDRESS RESULTS:", JSON.stringify(addresses));
+                return addresses;
+              });
+            },
+            (error) => {
+              console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000, accuracy: {android: "high", ios: "best" } }
+        );
+      } else {
+        return null;
+      }
+    })
+  }
 }
 
 const styles = StyleSheet.create({
